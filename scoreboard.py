c8f0fa552436f0d7ca2166295a6fff477ed150fe
_sohatzky <68334609+KuBuSxKuBa@users.noreply.github.com>
2022-04-21 18:39:59 +0200

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -1,24 +1,51 @@
 from turtle import Turtle
-SCORE = 0
+ALIGNMENT = "center"
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
         self.color("white")
+        self.score = 0
+        self.high_score = 0
         self.penup()
         self.hideturtle()
         self.goto(0 , 260)
-        self.write(f"Score : {SCORE}" , False , align="center" , font=("Arial" , 20))
+        self.update_scoreboard()
 
 
-    def add_point(self):
-        global SCORE
+    # def add_point(self):
+    #     self.clear()
+    #     self.score += 1
+    #     self.write(f"Score : {self.score}", False, align="center", font=("Arial", 20))
+    #
+    # # def game_over(self):
+    #     # global SCORE
+    #     # self.clear()
+    #     # self.goto(0 , 0)
+    #     # self.write(f"Game over! Total score: {SCORE}" , False, align="center", font=("Arial", 20))
+    #
+    # def reset(self):
+    #     if self.score > self.high_score:
+    #         self.high_score = self.score
+    #     self.score = 0
+    #     self.clear()
+    #     self.write(f"Score : {self.score} High score : {self.high_score}", False, align="center", font=("Arial", 20))
+
+    def update_scoreboard(self):
         self.clear()
-        SCORE += 1
-        self.write(f"Score : {SCORE}", False, align="center", font=("Arial", 20))
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT)
 
-    def game_over(self):
-        global SCORE
-        self.clear()
-        self.goto(0 , 0)
-        self.write(f"Game over! Total score: {SCORE}" , False, align="center", font=("Arial", 20))
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
+
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
+
+    def save_highscore(self):
+        print(self.high_score)
+        with open("highscore.txt", "w") as file:
+            file.write(self.high_score)
